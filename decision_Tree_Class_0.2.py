75653191cbbf3f5f26d945f70ae87ebc71181f32
星辰漫游者 <39415743+xcmyz@users.noreply.github.com>
2018-10-20 01:00:06 +0800

    还有use_tree_predict方法待完成

--- decision_Tree_Class_0.2.py
+++ decision_Tree_Class_0.2.py
@@ -1,7 +1,9 @@
 import math
+import time
 
 
 class Decision_Tree:
+    'The data structure of decision tree is very nb'
 
     Tree = {}
 
@@ -40,12 +42,79 @@ class Decision_Tree:
 
         features_dict = {}
         H_features = {}
-        H_one_features = {}
+        H_features_condition = {}
+        G_features = {}
+        IGR_features = {}
+
         features_dict.fromkeys(features_name, 0.0)
         H_features.fromkeys(features_name, 0.0)
-        H_one_features.fromkeys(features_name, 0.0)
+        H_features_condition.fromkeys(features_name, 0.0)
+        G_features.fromkeys(features_name, 0.0)
+        IGR_features.fromkeys(features_name, 0.0)
 
         H_dataset = 0.0
+        count_list = [0 for n in range(features["label"])]
+        for i in range(len(train_data)):
+            num = train_data[i]["label"]
+            count_list[num] = count_list[num] + 1
+        for j in range(features["label"]):
+            mul_1 = count_list[j] / len(train_data)
+            mul_2 = math.log2(mul_1)
+            H_dataset = H_dataset + mul_1 * mul_2
+        H_dataset = 0 - H_dataset
+
+        for key in features:
+            if key != "label":
+                count_list_feature = [0 for n in range(features[key])]
+                for i in range(len(train_data)):
+                    num = train_data[i][key]
+                    count_list_feature[num] = count_list[num] + 1
+                for j in range(features[key]):
+                    mul_1 = count_list_feature[j] / len(train_data)
+                    mul_2 = math.log2(mul_1)
+                    H_features[key] = H_features[key] + mul_1 * mul_2
+                H_features[key] = 0 - H_features[key]
+            else:
+                continue
+
+        for key in features:
+            if key != "label":
+                for a in range(features[key]):
+                    H_dataset_temp = 0.0
+                    count_list_temp = [0 for n in range(features["label"])]
+                    cnt_i = 0
+                    for i in range(len(train_data)):
+                        if train_data[i][key] == a:
+                            cnt_i = cnt_i + 1
+                            num = train_data[i]["label"]
+                            count_list_temp[num] = count_list_temp[num] + 1
+                    for j in range(features["label"]):
+                        mul_1 = count_list_temp[j] / cnt_i
+                        mul_2 = math.log2(mul_1)
+                        H_dataset_temp = H_dataset_temp + mul_1 * mul_2
+                    H_dataset_temp = 0 - H_dataset_temp
+                    H_features_condition[key] = H_features_condition[key] + \
+                        (cnt_i / len(train_data)) * H_dataset_temp
+                H_features_condition[key] = 0 - H_features_condition[key]
+            else:
+                continue
+
+        for key_name_1 in G_features:
+            G_features[key_name_1] = H_dataset - \
+                H_features_condition[key_name_1]
+
+        for key_name_2 in IGR_features:
+            IGR_features[key_name_2] = G_features[key_name_2] / \
+                H_features[key_name_2]
+
+        out_feature = ""
+        out_IGR = 0.0
+        for key_name_3 in IGR_features:
+            if out_IGR <= IGR_features[key_name_3]:
+                out_IGR = IGR_features[key_name_3]
+                out_feature = key_name_3
+
+        return out_feature, out_IGR
 
     def build_tree(train_data, features, decision_tree):
         # 训练集要包含如下元素：特征名称和特征值，为一个列表嵌套字典
@@ -53,6 +122,8 @@ class Decision_Tree:
         # 因为这个程序为一个递归程序，所以decision_tree这个传过来的参数
         # 可能仅仅是树的一个分支，经过ipython的test
         # python中的传值皆为对象，相当于一个引用
+
+        t1 = time.clock()
 
         ifequal = True
         flag = train_data[0]["label"]
@@ -95,3 +166,10 @@ class Decision_Tree:
             decision_tree[feature]
             build_tree(processed_train_data, processed_features,
                        decision_tree[feature][cnt])
+
+        t2 = time.clock()
+        print("Time used: %f" % (t2 - t1))
+
+    def use_tree_predict(test_data):
+        out_label = 0
+        return out_label
